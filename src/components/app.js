741f81950a8a1c7eb9d3653f9e527b6c3a5ed2f1
Autumn <autumncbullard@gmail.com>
2017-10-17 15:27:50 -0400

    Create a header and a footer

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,13 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid"></div>
+      <div className="container-fluid">
+        <header className="text-center">
+          <h1>freeCodeCamp <span className="fa fa-free-code-camp"></span></h1>
+          <h2><span className="fa fa-trophy"></span> Leaderboard</h2>
+        </header>
+        <footer className="text-center">Coded by <a href="../portfolio" target="_blank">Autumn Bullard</a></footer>
+      </div>
     );
   }
 }
