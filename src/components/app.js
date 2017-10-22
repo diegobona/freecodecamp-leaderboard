5ecdb557b5fb041d5dc74b95aaf3f036b9fe41db
Autumn <autumncbullard@gmail.com>
2017-10-22 17:18:05 -0400

    Replace text in h1 tag with an image

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+const logo = require('.././images/freecodecamp-logo.png');
+
 export default class App extends Component {
 
   render() {
     return (
       <div className="container-fluid">
         <header className="text-center">
-          <h1>freeCodeCamp <span className="fa fa-free-code-camp"></span></h1>
+          <h1><img src={logo} alt="freeCodeCamp" /></h1>
           <h2><span className="fa fa-trophy"></span> Leaderboard</h2>
         </header>
         <footer className="text-center">Coded by <a href="../portfolio" target="_blank">Autumn Bullard</a></footer>
