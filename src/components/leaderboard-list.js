565923d5f298494ffa59e5e4c276894e29c397df
Autumn <autumncbullard@gmail.com>
2017-10-25 16:23:17 -0400

    Build leaderboard table

--- src/components/leaderboard-list.js
+++ src/components/leaderboard-list.js
@@ -3,6 +3,17 @@ import React from 'react';
 const LeaderboardList = ({ campers }) => {
 
   return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Camper</th>
+          <th>30 Days</th>
+          <th>All Time</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
   );
 }
 
