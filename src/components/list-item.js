a54fb1afcaa1c5aa36db02b3fb4b63acdf5c76bb
Autumn <autumncbullard@gmail.com>
2017-10-25 17:39:09 -0400

    Build the tr that will be called for each camper in the array

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const ListItem = (props) => {
+const ListItem = ({ rank, camper }) => {
 
   return (
+    <tr>
+      <td>{rank}</td>
+      <td>
+        <img src={camper.img} alt={`profile picture for ${camper.username}`} />
+        <a href={`https://www.freecodecamp.org/${camper.username}`} target="_blank">{camper.username}</a>
+      </td>
+      <td>{camper.recent}</td>
+      <td>{camper.alltime}</td>
+    </tr>
   );
 }
 
